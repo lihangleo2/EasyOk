6a317a0d26f971fb19f77493d25c24bcb8ac76f4
leo <lihangleo2@163.com>
2019-09-05 16:26:52 +0800

    Update LogUtils.java

--- app/src/main/java/com/lihang/selfmvc/utils/LogUtils.java
+++ app/src/main/java/com/lihang/selfmvc/utils/LogUtils.java
@@ -9,118 +9,204 @@ import android.util.Log;
 
 public class LogUtils {
 
-    //打印日志开光，false则不打印
-    private static boolean isPrintLog = true;
-    private static int LOG_MAXLENGTH = 2000;
+    //可以全局控制是否打印log日志
+    private static boolean isEnableLog = true;
+    private static int LOG_MAXLENGTH = 4000;
+    public static final String TAG = LogUtils.class.getSimpleName();
+    private static final String TOP_DIVIDER =
+            "┌────────────────────────────────────────────────────────";
 
+    private static final String BOTTOM_DIVIDER =
+            "└────────────────────────────────────────────────────────";
+    private static final String MIDDLE_DIVIDER = "----------- 换行 ------------\n";
 
     public static void v(String msg) {
-        v("LogUtil", msg);
+        v(TAG, msg);
     }
 
     public static void v(String tagName, String msg) {
-        if (isPrintLog) {
+        if (isEnableLog) {
+            if (msg == null) {
+                Log.v(tagName, "null");
+                return;
+            }
             int strLength = msg.length();
             int start = 0;
             int end = LOG_MAXLENGTH;
-            for (int i = 0; i < 100; i++) {
-                if (strLength > end) {
-                    Log.v(tagName + i, msg.substring(start, end));
-                    start = end;
-                    end = end + LOG_MAXLENGTH;
-                } else {
-                    Log.v(tagName + i, msg.substring(start, strLength));
-                    break;
+            if (strLength > end) {
+                StringBuffer sbf = new StringBuffer();
+                sbf.append(" \n" + TOP_DIVIDER).append(msg);
+                String trueMsg = sbf.toString();
+                strLength = trueMsg.length();
+                while (strLength > end) {
+                    if (start == 0) {
+                        Log.v(tagName, trueMsg.substring(start, end));
+                        start = end;
+                        end = end + LOG_MAXLENGTH;
+                    } else {
+                        end = end - MIDDLE_DIVIDER.length();
+                        Log.v(tagName, MIDDLE_DIVIDER + trueMsg.substring(start, end));
+                        start = end;
+                        end = end + LOG_MAXLENGTH;
+                    }
                 }
+                Log.v(tagName, trueMsg.substring(start, strLength));
+                Log.v(tagName, " \n" + BOTTOM_DIVIDER);
+            } else {
+                Log.v(tagName, msg);
             }
         }
     }
 
     public static void d(String msg) {
-        d("LogUtil", msg);
+        d(TAG, msg);
     }
 
     public static void d(String tagName, String msg) {
-        if (isPrintLog) {
+        if (isEnableLog) {
+            if (msg == null) {
+                Log.d(tagName, "null");
+                return;
+            }
             int strLength = msg.length();
             int start = 0;
             int end = LOG_MAXLENGTH;
-            for (int i = 0; i < 100; i++) {
-                if (strLength > end) {
-                    Log.d(tagName + i, msg.substring(start, end));
-                    start = end;
-                    end = end + LOG_MAXLENGTH;
-                } else {
-                    Log.d(tagName + i, msg.substring(start, strLength));
-                    break;
+            if (strLength > end) {
+                StringBuffer sbf = new StringBuffer();
+                sbf.append(" \n" + TOP_DIVIDER).append(msg);
+                String trueMsg = sbf.toString();
+                strLength = trueMsg.length();
+                while (strLength > end) {
+                    if (start == 0) {
+                        Log.d(tagName, trueMsg.substring(start, end));
+                        start = end;
+                        end = end + LOG_MAXLENGTH;
+                    } else {
+                        end = end - MIDDLE_DIVIDER.length();
+                        Log.d(tagName, MIDDLE_DIVIDER + trueMsg.substring(start, end));
+                        start = end;
+                        end = end + LOG_MAXLENGTH;
+                    }
                 }
+                Log.d(tagName, trueMsg.substring(start, strLength));
+                Log.d(tagName, " \n" + BOTTOM_DIVIDER);
+            } else {
+                Log.d(tagName, msg);
             }
         }
     }
 
     public static void i(String msg) {
-        i("LogUtil", msg);
+        i(TAG, msg);
     }
 
+
     public static void i(String tagName, String msg) {
-        if (isPrintLog) {
+        if (isEnableLog) {
+            if (msg == null) {
+                Log.i(tagName, "null");
+                return;
+            }
             int strLength = msg.length();
             int start = 0;
             int end = LOG_MAXLENGTH;
-            for (int i = 0; i < 100; i++) {
-                //
-                if (strLength > end) {
-                    Log.i(tagName, msg.substring(start, end));
-                    start = end;
-                    end = end + LOG_MAXLENGTH;
-                } else {
-                    Log.i(tagName, msg.substring(start, strLength));
-                    break;
+            if (strLength > end) {
+                StringBuffer sbf = new StringBuffer();
+                sbf.append(" \n" + TOP_DIVIDER).append(msg);
+                String trueMsg = sbf.toString();
+                strLength = trueMsg.length();
+                while (strLength > end) {
+                    if (start == 0) {
+                        Log.i(tagName, trueMsg.substring(start, end));
+                        start = end;
+                        end = end + LOG_MAXLENGTH;
+                    } else {
+                        end = end - MIDDLE_DIVIDER.length();
+                        Log.i(tagName, MIDDLE_DIVIDER + trueMsg.substring(start, end));
+                        start = end;
+                        end = end + LOG_MAXLENGTH;
+                    }
                 }
+                Log.i(tagName, trueMsg.substring(start, strLength));
+                Log.i(tagName, " \n" + BOTTOM_DIVIDER);
+            } else {
+                Log.i(tagName, msg);
             }
         }
     }
 
     public static void w(String msg) {
-        w("LogUtil", msg);
+        w(TAG, msg);
     }
 
     public static void w(String tagName, String msg) {
-        if (isPrintLog) {
+        if (isEnableLog) {
+            if (msg == null) {
+                Log.w(tagName, "null");
+                return;
+            }
             int strLength = msg.length();
             int start = 0;
             int end = LOG_MAXLENGTH;
-            for (int i = 0; i < 100; i++) {
-                if (strLength > end) {
-                    Log.w(tagName + i, msg.substring(start, end));
-                    start = end;
-                    end = end + LOG_MAXLENGTH;
-                } else {
-                    Log.w(tagName + i, msg.substring(start, strLength));
-                    break;
+            if (strLength > end) {
+                StringBuffer sbf = new StringBuffer();
+                sbf.append(" \n" + TOP_DIVIDER).append(msg);
+                String trueMsg = sbf.toString();
+                strLength = trueMsg.length();
+                while (strLength > end) {
+                    if (start == 0) {
+                        Log.w(tagName, trueMsg.substring(start, end));
+                        start = end;
+                        end = end + LOG_MAXLENGTH;
+                    } else {
+                        end = end - MIDDLE_DIVIDER.length();
+                        Log.w(tagName, MIDDLE_DIVIDER + trueMsg.substring(start, end));
+                        start = end;
+                        end = end + LOG_MAXLENGTH;
+                    }
                 }
+                Log.w(tagName, trueMsg.substring(start, strLength));
+                Log.w(tagName, " \n" + BOTTOM_DIVIDER);
+            } else {
+                Log.w(tagName, msg);
             }
         }
     }
 
     public static void e(String msg) {
-        e("LogUtil", msg);
+        e(TAG, msg);
     }
 
     public static void e(String tagName, String msg) {
-        if (isPrintLog) {
+        if (isEnableLog) {
+            if (msg == null) {
+                Log.e(tagName, "null");
+                return;
+            }
             int strLength = msg.length();
             int start = 0;
             int end = LOG_MAXLENGTH;
-            for (int i = 0; i < 100; i++) {
-                if (strLength > end) {
-                    Log.e(tagName + i, msg.substring(start, end));
-                    start = end;
-                    end = end + LOG_MAXLENGTH;
-                } else {
-                    Log.e(tagName + i, msg.substring(start, strLength));
-                    break;
+            if (strLength > end) {
+                StringBuffer sbf = new StringBuffer();
+                sbf.append(" \n" + TOP_DIVIDER).append(msg);
+                String trueMsg = sbf.toString();
+                strLength = trueMsg.length();
+                while (strLength > end) {
+                    if (start == 0) {
+                        Log.e(tagName, trueMsg.substring(start, end));
+                        start = end;
+                        end = end + LOG_MAXLENGTH;
+                    } else {
+                        end = end - MIDDLE_DIVIDER.length();
+                        Log.e(tagName, MIDDLE_DIVIDER + trueMsg.substring(start, end));
+                        start = end;
+                        end = end + LOG_MAXLENGTH;
+                    }
                 }
+                Log.e(tagName, trueMsg.substring(start, strLength));
+                Log.e(tagName, " \n" + BOTTOM_DIVIDER);
+            } else {
+                Log.e(tagName, msg);
             }
         }
     }
